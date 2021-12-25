8851a7c441764387de79ffd784d89ece9050346d
Samuel Kusancke <ceifmode@gmail.com>
2021-12-25 19:52:13 -0300

    some changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask
 import os
 app = flask.Flask(__name__, static_url_path='/static', static_folder='static')
 
-PORT = os.environ.get('PORT') or 3000
+PORT = 3000
 
 
 @app.route('/', methods=['GET', 'POST'])
